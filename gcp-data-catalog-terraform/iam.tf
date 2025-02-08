ff012fbcfbfba778c2433362a5c9d55baaff1687
Diana Tahchieva <diana.tahchieva@gmail.com>
2025-02-08 23:17:36 +0100

    added explanation on how the sa was created

--- gcp-data-catalog-terraform/iam.tf
+++ gcp-data-catalog-terraform/iam.tf
@@ -1,3 +1,9 @@
+#Run this command to create zour service account:
+#gcloud iam service-accounts create terraform-sa --display-name "Terraform Service Account" --project gcp-data-governance
+#Run this command to get the email of your newly created service account:
+#gcloud iam service-accounts list --filter="displayName:Terraform Service Account" --format="value(email)"
+
+
 # Give the Terraform user permissions to manage IAM, Dataplex, and BigQuery
 resource "google_project_iam_member" "terraform_bigquery_admin" {
   project = var.project_id
